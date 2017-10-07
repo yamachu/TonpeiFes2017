87fc546e4e36a5e563b4ec28e0a98e129404f720
Yusuke Yamada <yamachu.dev@gmail.com>
2017-10-07 23:55:47 +0900

    Initial commit

---
+++ TonpeiFes/App.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Prism.Unity;
+using Xamarin.Forms;
+
+namespace TonpeiFes
+{
+    public partial class App : PrismApplication
+    {
+        public App(IPlatformInitializer initializer = null) : base(initializer) { }
+
+        protected override void OnInitialized()
+        {
+            InitializeComponent();
+        }
+
+        protected override void RegisterTypes()
+        {
+        }
+    }
+}
---
+++ iOS/AppDelegate.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Foundation;
+using Microsoft.Practices.Unity;
+using Prism.Unity;
+using UIKit;
+
+namespace TonpeiFes.iOS
+{
+    [Register("AppDelegate")]
+    public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
+    {
+        public override bool FinishedLaunching(UIApplication app, NSDictionary options)
+        {
+            global::Xamarin.Forms.Forms.Init();
+
+            LoadApplication(new App(new iOSInitializer()));
+
+            return base.FinishedLaunching(app, options);
+        }
+    }
+
+    public class iOSInitializer : IPlatformInitializer
+    {
+        public void RegisterTypes(IUnityContainer container)
+        {
+
+        }
+    }
+}
