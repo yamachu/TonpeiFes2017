0453813db4cc4d489a7c15426e109948319eca07
Yusuke Yamada <yamachu.dev@gmail.com>
2017-10-12 22:38:17 +0900

    StageEventの最低限の見栄えの調整

--- TonpeiFes/ViewModels/Pages/StageEventListRootPageViewModel.cs
+++ TonpeiFes/ViewModels/Pages/StageEventListRootPageViewModel.cs
@@ -3,12 +3,18 @@ using Prism.Mvvm;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using Reactive.Bindings;
 
 namespace TonpeiFes.ViewModels.Pages
 {
     public class StageEventListRootPageViewModel : ViewModelBase
     {
         public string Title => "ステージイベント";
+        public string Day1Segment => "11/3";
+        public string Day2Segment => "11/4";
+        public string Day3Segment => "11/5";
+
+        public ReactiveProperty<int> SelectedSegment { get; } = new ReactiveProperty<int>(0);
         public StageEventListRootPageViewModel()
         {
 
