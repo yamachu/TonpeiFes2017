b6e6ae6784b66da3a6272420deed1e8339c71d5d
Yusuke Yamada <yamachu.dev@gmail.com>
2017-10-12 17:35:31 +0900

    お気に入りボタンの動作確認のコードの追加

--- TonpeiFes/ViewModels/Pages/PlanningListRootPageViewModel.cs
+++ TonpeiFes/ViewModels/Pages/PlanningListRootPageViewModel.cs
@@ -5,6 +5,8 @@ using System.Collections.Generic;
 using System.Linq;
 using Reactive.Bindings;
 using System.Reactive.Linq;
+using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace TonpeiFes.ViewModels.Pages
 {
@@ -12,11 +14,16 @@ namespace TonpeiFes.ViewModels.Pages
     {
         // ToDo: 文字列リソースはどこかにまとめる
         public string Title => "模擬店/企画";
-        public string OutdoorSegment { get; } = "屋外";
-        public string IndoorSegment { get; } = "屋内";
+        public string OutdoorSegment => "屋外";
+        public string IndoorSegment => "屋内";
 
         public ReactiveProperty<int> SelectedSegment { get; } = new ReactiveProperty<int>(0);
         public ReactiveProperty<string> SearchQuery { get; } = new ReactiveProperty<string>();
+
+        public ReadOnlyReactiveProperty<string> IconSource { get; }
+        public ICommand FavButtonClickCommand { get; }
+        private ReactiveProperty<bool> FavStateObservable = new ReactiveProperty<bool>(false);
+        public bool IsAndroid => Device.RuntimePlatform == Device.Android;
 
         public PlanningListRootPageViewModel()
         {
@@ -30,6 +37,16 @@ namespace TonpeiFes.ViewModels.Pages
             {
                 System.Diagnostics.Debug.WriteLine($"Query Update: {query}");
             });
+
+            FavButtonClickCommand = new DelegateCommand(() =>
+            {
+                FavStateObservable.Value = !FavStateObservable.Value;
+            });
+
+            IconSource = FavStateObservable.Select((isFavActive) =>
+            {
+                return $@"ion-ios-heart{(isFavActive ? "" : "-outline")}";
+            }).ToReadOnlyReactiveProperty("ion-ios-heart");
         }
     }
 }
