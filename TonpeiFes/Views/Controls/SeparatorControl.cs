71ca28043d1970d790dcd61b094338a32bdd9419
Yusuke Yamada <yamachu.dev@gmail.com>
2017-10-12 17:33:00 +0900

    区切り線専用のコントロールの追加

---
+++ TonpeiFes/Views/Controls/SeparatorControl.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace TonpeiFes.Views.Controls
+{
+    // https://forums.xamarin.com/discussion/47896/how-to-draw-a-separator
+    public class SeparatorControl : BoxView
+    {
+        public SeparatorControl()
+        {
+            VerticalOptions = LayoutOptions.FillAndExpand;
+            HeightRequest = 1;
+        }
+    }
+}
