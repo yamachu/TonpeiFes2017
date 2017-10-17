c898c26275d5384f8bc96f4fb0dcfea23c4d0b1b
Yusuke Yamada <yamachu.dev@gmail.com>
2017-10-18 03:00:33 +0900

    アイテムのグループ化のヘルパーを追加

---
+++ TonpeiFes.MobileCore/Helpers/ObservableGroupCollection.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace TonpeiFes.MobileCore.Helpers
+{
+    // https://mallibone.com/post/xamarin.forms-grouped-listview
+    public class ObservableGroupCollection<S, T> : ObservableCollection<T>
+    {
+        private readonly S _key;
+
+        public ObservableGroupCollection(IGrouping<S, T> group)
+            : base(group)
+        {
+            _key = group.Key;
+        }
+
+        public S Key
+        {
+            get { return _key; }
+        }
+    }
+}
