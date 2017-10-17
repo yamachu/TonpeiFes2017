15abd4d050f208a75bc668b77b06bf27ab0b5050
Yusuke Yamada <yamachu.dev@gmail.com>
2017-10-18 03:41:43 +0900

    StageEventのページのViewModelをいい感じにした

--- TonpeiFes.MobileCore/ViewModels/Pages/StageEventListRootPageViewModel.cs
+++ TonpeiFes.MobileCore/ViewModels/Pages/StageEventListRootPageViewModel.cs
@@ -1,9 +1,12 @@
-﻿using Prism.Commands;
-using Prism.Mvvm;
-using System;
-using System.Collections.Generic;
+﻿using System;
 using System.Linq;
+using System.Reactive.Linq;
+using System.Windows.Input;
+using Prism.Commands;
 using Reactive.Bindings;
+using TonpeiFes.MobileCore.Helpers;
+using TonpeiFes.MobileCore.Models.DataObjects;
+using TonpeiFes.MobileCore.Usecases;
 
 namespace TonpeiFes.MobileCore.ViewModels.Pages
 {
@@ -15,9 +18,34 @@ namespace TonpeiFes.MobileCore.ViewModels.Pages
         public string Day3Segment => "11/5";
 
         public ReactiveProperty<int> SelectedSegment { get; } = new ReactiveProperty<int>(0);
-        public StageEventListRootPageViewModel()
-        {
+        public ReadOnlyReactiveProperty<string> IconSource { get; }
+        public ICommand FavButtonClickCommand { get; }
+        public ReadOnlyReactiveCollection<ObservableGroupCollection<string, ISearchableListPlanning>> Plannings { get; }
 
+        private ReactiveProperty<bool> FavStateObservable = new ReactiveProperty<bool>(false);
+        private IFilterGroupingStageEvent _eventUsecase;
+
+        public StageEventListRootPageViewModel(IFilterGroupingStageEvent eventUsecase)
+        {
+            _eventUsecase = eventUsecase;
+
+            SelectedSegment.Subscribe(selectedSegment =>
+            {
+                _eventUsecase.UpdateFilterConditions(SelectedSegment.Value, FavStateObservable.Value);
+            });
+
+            FavButtonClickCommand = new DelegateCommand(() =>
+            {
+                FavStateObservable.Value = !FavStateObservable.Value;
+                _eventUsecase.UpdateFilterConditions(SelectedSegment.Value, FavStateObservable.Value);
+            });
+
+            IconSource = FavStateObservable.Select((isFavActive) =>
+            {
+                return $@"ion-ios-heart{(isFavActive ? "" : "-outline")}";
+            }).ToReadOnlyReactiveProperty("ion-ios-heart");
+
+            Plannings = _eventUsecase.Plannings.ToReadOnlyReactiveCollection();
         }
     }
 }
