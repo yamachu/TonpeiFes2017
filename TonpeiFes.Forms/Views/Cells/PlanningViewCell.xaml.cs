de449c4b6e5f4809f9a768774bafe5d1ea9cc8e4
Yusuke Yamada <yamachu.dev@gmail.com>
2017-10-18 23:09:57 +0900

    展示リストのアイテムを表示するセルのテンプレートを作成

---
+++ TonpeiFes.Forms/Views/Cells/PlanningViewCell.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace TonpeiFes.Forms.Views.Cells
+{
+    public partial class PlanningViewCell : ViewCell
+    {
+        public PlanningViewCell()
+        {
+            InitializeComponent();
+        }
+    }
+}
