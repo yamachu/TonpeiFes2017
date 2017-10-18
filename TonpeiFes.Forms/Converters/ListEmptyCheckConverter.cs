41f16df87eb10971f4dd9d982467db54e9de1fdb
Yusuke Yamada <yamachu.dev@gmail.com>
2017-10-18 21:16:36 +0900

    Listをバインドするためのヘルパーを追加

--- TonpeiFes.Forms/Converters/ListEmptyCheckConverter.cs
+++ TonpeiFes.Forms/Converters/ListEmptyCheckConverter.cs
@@ -1,20 +1,21 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Globalization;
-using System.Linq;
 using Xamarin.Forms;
 
 namespace TonpeiFes.Forms.Converters
 {
-    public class KeywordListJoinConverter : IValueConverter
+    public class ListEmptyCheckConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            if (value == null || !(value is IList<string>))
-            {
-                return default(string);
-            }
-            return ((IList<string>)value)?.Aggregate((acc, next) => $"{acc}, {next}");
+            if (value == null || !(value is IList || value is IList<object>)) return true;
+            var genericListObj = value as IList<object>;
+            if (genericListObj != null) return genericListObj.Count == 0;
+            var listObj = value as IList;
+            if (genericListObj != null) return listObj.Count == 0;
+            return true;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
