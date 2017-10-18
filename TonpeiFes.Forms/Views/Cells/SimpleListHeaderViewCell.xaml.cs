1f5bebb8f21755342eed2880ca74ac8d271254dc
Yusuke Yamada <yamachu.dev@gmail.com>
2017-10-18 15:54:05 +0900

    ListのヘッダーのCellの追加

---
+++ TonpeiFes.Forms/Views/Cells/SimpleListHeaderViewCell.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace TonpeiFes.Forms.Views.Cells
+{
+    public partial class SimpleListHeaderViewCell : ContentView
+    {
+        public SimpleListHeaderViewCell()
+        {
+            InitializeComponent();
+        }
+    }
+}
