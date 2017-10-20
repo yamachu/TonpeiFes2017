c16e65880019018ce38fef40fae518eac63450b9
Yusuke Yamada <yamachu.dev@gmail.com>
2017-10-21 07:37:22 +0900

    便利Converterの追加

---
+++ TonpeiFes.Forms/Converters/IsNullOrEmptyOrWhitespaceConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace TonpeiFes.Forms.Converters
+{
+    public class IsNullOrEmptyOrWhitespaceConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null || !(value is string)) return true;
+            return string.IsNullOrEmpty(value as string) || string.IsNullOrWhiteSpace(value as string);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
