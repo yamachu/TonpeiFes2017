227e30c8ee40f0f7f8e3fe660d566340711d768d
Yusuke Yamada <yamachu.dev@gmail.com>
2017-10-23 02:09:30 +0900

    HeaderGroupをいい感じにするためのモックを追加

---
+++ TonpeiFes.Forms/Views/Cells/IconedListHeaderViewCell.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace TonpeiFes.Forms.Views.Cells
+{
+    public partial class IconedListHeaderViewCell : ContentView
+    {
+        public IconedListHeaderViewCell()
+        {
+            InitializeComponent();
+        }
+    }
+}
