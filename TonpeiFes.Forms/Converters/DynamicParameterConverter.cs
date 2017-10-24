b7961e1588d3f361dfae26efec6159d769e9ab70
Yusuke Yamada <yamachu.dev@gmail.com>
2017-10-24 10:09:48 +0900

    SNSのアイコンを表示するかの判定ロジックを追加

---
+++ TonpeiFes.Forms/Converters/DynamicParameterConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+using Microsoft.CSharp.RuntimeBinder;
+using Xamarin.Forms;
+
+namespace TonpeiFes.Forms.Converters
+{
+    public class DynamicParameterConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+            {
+                return null;
+            }
+            dynamic _value = value;
+
+            try
+            {
+                var propertyInfo = _value.GetType().GetProperty(parameter as string);
+                return propertyInfo.GetValue(_value, null) as string;
+            }
+            catch(RuntimeBinderException ex)
+            {
+                return null;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
