819fa0dc2e7651af2cb2fae1e856bb032139413e
Yusuke Yamada <yamachu.dev@gmail.com>
2017-10-25 03:03:24 +0900

    OnNavigationToが使えないことへの対策としてのEventArgsを追加

---
+++ TonpeiFes.MobileCore/Models/EventArgs/TabbedPageOpendEventArgs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Prism.Events;
+
+namespace TonpeiFes.MobileCore.Models.EventArgs
+{
+    public class TabbedPageOpendEventArgs
+    {
+        public string Name { get; private set; }
+
+        public TabbedPageOpendEventArgs(string pageName)
+        {
+            Name = pageName;
+        }
+    }
+
+    public class TabbedPageOpendEvent : PubSubEvent<TabbedPageOpendEventArgs> { }
+}
