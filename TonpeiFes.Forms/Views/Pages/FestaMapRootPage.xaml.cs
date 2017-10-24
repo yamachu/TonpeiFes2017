7aac02fecf0e9076387b97d77ade77c5f870dd16
Yusuke Yamada <yamachu.dev@gmail.com>
2017-10-25 03:05:44 +0900

    いい感じにマップを実装
    ToDo: 個別ページへの対応

--- TonpeiFes.Forms/Views/Pages/FestaMapRootPage.xaml.cs
+++ TonpeiFes.Forms/Views/Pages/FestaMapRootPage.xaml.cs
@@ -1,6 +1,11 @@
-﻿using Microsoft.Practices.Unity;
+﻿using System;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using Microsoft.Practices.Unity;
 using Prism.Events;
+using Reactive.Bindings;
 using TonpeiFes.MobileCore.Models.EventArgs;
+using TonpeiFes.MobileCore.ViewModels.Pages;
 using Xamarin.Forms;
 using Xamarin.Forms.GoogleMaps;
 
@@ -9,6 +14,8 @@ namespace TonpeiFes.Forms.Views.Pages
     public partial class FestaMapRootPage : ContentPage
     {
         private IEventAggregator _eventAggregator;
+
+        private IDisposable _allDispose;
 
         public FestaMapRootPage()
         {
@@ -27,7 +34,35 @@ namespace TonpeiFes.Forms.Views.Pages
                         args.IsInitialPosition ? Configurations.ConstParameters.MAP_CENTER_Y : args.Latitude),
                     Distance.FromMeters(100)), false);
             });
+
+            var pinDisposable = (BindingContext as FestaMapRootPageViewModel)
+                .Pins.ToCollectionChanged<Pin>()
+                .Subscribe(pinChange =>
+            {
+                if (pinChange.Action == System.Collections.Specialized.NotifyCollectionChangedAction.Add)
+                {
+                    map.Pins.Add(pinChange.Value);
+                }
+            });
+
+            var polygonDisposable = (BindingContext as FestaMapRootPageViewModel)
+                .Polygons.ToCollectionChanged<Polygon>()
+                .Subscribe(polygonChange =>
+            {
+                if (polygonChange.Action == System.Collections.Specialized.NotifyCollectionChangedAction.Add)
+                {
+                    map.Polygons.Add(polygonChange.Value);
+                }
+            });
+
+            _allDispose = new CompositeDisposable(pinDisposable, polygonDisposable);
+        }
+
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+
+            _allDispose.Dispose();
         }
     }
 }
-
--- TonpeiFes.MobileCore/ViewModels/Pages/FestaMapRootPageViewModel.cs
+++ TonpeiFes.MobileCore/ViewModels/Pages/FestaMapRootPageViewModel.cs
@@ -1,22 +1,94 @@
-﻿using Prism.Commands;
-using Prism.Mvvm;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Prism.Commands;
 using Prism.Events;
+using Prism.Navigation;
+using Reactive.Bindings;
 using TonpeiFes.MobileCore.Models.EventArgs;
+using TonpeiFes.MobileCore.Usecases;
+using Xamarin.Forms.GoogleMaps;
 
 namespace TonpeiFes.MobileCore.ViewModels.Pages
 {
     public class FestaMapRootPageViewModel : ViewModelBase
     {
-        private IEventAggregator _eventAggregator;
+        private readonly static string NavigationKey = "NavigationKey";
 
-        public FestaMapRootPageViewModel(IEventAggregator eventAggregator)
+        public ReadOnlyObservableCollection<Pin> Pins { get; }
+        public ReadOnlyObservableCollection<Polygon> Polygons { get; }
+
+        public ICommand InfoWindowClickedCommand { get; }
+        public ReactiveProperty<Pin> SelectedPin { get; } = new ReactiveProperty<Pin>();
+        public ReactiveProperty<bool> IsShowingUser { get; } = new ReactiveProperty<bool>(false);
+
+        private IEventAggregator _eventAggregator;
+        private INavigationService _navigationService;
+        private IShowFestaMap showFestaUsecase;
+
+        private bool IsGlobalMap = true;
+        private bool IsInitialized = false;
+
+        public FestaMapRootPageViewModel(INavigationService navigationService, IEventAggregator eventAggregator, IShowFestaMap showFesta)
         {
             _eventAggregator = eventAggregator;
+            _navigationService = navigationService;
+            showFestaUsecase = showFesta;
 
-            _eventAggregator.GetEvent<MapMoveEvent>().Publish(new MapMoveEventArgs(0, 0, true));
+            InfoWindowClickedCommand = new DelegateCommand<Pin>((pin) =>
+            {
+                if (IsGlobalMap)
+                {
+                    //_navigationService.NavigateAsync();
+                }
+            });
+
+            _eventAggregator.GetEvent<PolygonClickedEvent>().Subscribe((pin) =>
+            {
+                SelectedPin.Value = pin.Tag as Pin;
+            });
+
+            Pins = showFesta.Pins.ToReadOnlyReactiveCollection();
+            Polygons = showFesta.Polygons.ToReadOnlyReactiveCollection();
+
+            // For iOS
+            _eventAggregator.GetEvent<TabbedPageOpendEvent>().Subscribe((ev) =>
+            {
+                if (ev.Name != this.GetType().Name.Replace("ViewModel", "")) return;
+
+                IsShowingUser.Value = true;
+                if (IsGlobalMap)
+                {
+                    _eventAggregator.GetEvent<MapMoveEvent>().Publish(new MapMoveEventArgs(0, 0, true));
+                    if (!IsInitialized)
+                    {
+                        showFestaUsecase.InitializeAllMapObjects();
+                        IsInitialized = true;
+                    }
+                }
+            });
+
+            _eventAggregator.GetEvent<LocationPermissionRequestResultEvent>()
+                            .Subscribe((ev) =>
+            {
+                IsShowingUser.Value = ev.Granted;
+            });
+
+        }
+
+        // iOS: called with parameter only
+        // Android: always called
+        public override void OnNavigatingTo(NavigationParameters parameters)
+        {
+            base.OnNavigatingTo(parameters);
+
+            // For Android
+            if (parameters == null || !parameters.ContainsKey(NavigationKey))
+            {
+                _eventAggregator.GetEvent<MapMoveEvent>().Publish(new MapMoveEventArgs(0, 0, true));
+                showFestaUsecase.InitializeAllMapObjects();
+                _eventAggregator.GetEvent<LocationPermissionRequestEvent>().Publish(new LocationPermissionRequestEventArgs());
+            }
         }
     }
 }
