4b317d3afc8b3c4e00fd8af6de8d483fda9b55b6
Yusuke Yamada <yamachu.dev@gmail.com>
2017-10-26 01:39:47 +0900

    [WIP] HomePageの最低限のUsecaseを追加

---
+++ TonpeiFes.MobileCore/Usecases/IShowAnnouncement.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using TonpeiFes.Core.Models.DataObjects;
+
+namespace TonpeiFes.MobileCore.Usecases
+{
+    public interface IShowAnnouncement
+    {
+        ReadOnlyObservableCollection<Announcement> Announcements { get; }
+
+        Task InitializeAnnouncements();
+    }
+}
---
+++ TonpeiFes.MobileCore/Usecases/ShowAnnouncement.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using System.Linq;
+using TonpeiFes.Core.Models.DataObjects;
+using TonpeiFes.MobileCore.Repositories;
+
+namespace TonpeiFes.MobileCore.Usecases
+{
+    public class ShowAnnouncement : IShowAnnouncement
+    {
+        private readonly ObservableCollection<Announcement> _announcements = new ObservableCollection<Announcement>();
+        public ReadOnlyObservableCollection<Announcement> Announcements { get; }
+
+        private IRepository<Announcement> _announcementRepository;
+
+        public ShowAnnouncement(IRepository<Announcement> announceRep)
+        {
+            _announcementRepository = announceRep;
+
+            Announcements = new ReadOnlyObservableCollection<Announcement>(_announcements);
+        }
+
+        public async Task InitializeAnnouncements()
+        {
+            foreach (var item in _announcementRepository.GetAll().OrderBy(item => item.Index))
+            {
+                _announcements.Add(item);
+            }
+        }
+    }
+}
