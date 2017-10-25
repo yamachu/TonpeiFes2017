ae49824a1a87d9e0114b79145e189550fb728f39
Yusuke Yamada <yamachu.dev@gmail.com>
2017-10-26 06:24:13 +0900

    MobileCenterのAPIを格納する系のものを実装

---
+++ TonpeiFes.Forms/Configurations/MobileCenterAPI.tmpl.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace TonpeiFes.Forms.Configurations
+{
+    // When build this app, rename this file "MobileCenterAPI.cs"
+    public static class MobileCenterAPI
+    {
+        public const string MOBILE_CENTER_ANDROID_API_KEY = "replace!";
+        public const string MOBILE_CENTER_IOS_API_KEY = "replace!";
+    }
+}
