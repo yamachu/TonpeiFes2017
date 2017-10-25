9161b21d39d4a1ee5fb318e68fafec1a16ed14d7
Yusuke Yamada <yamachu.dev@gmail.com>
2017-10-26 01:36:47 +0900

    Announcementのモデル定義を追加

---
+++ TonpeiFes.Core/Models/DataObjects/Announcement.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Realms;
+
+namespace TonpeiFes.Core.Models.DataObjects
+{
+    public class Announcement : RealmObject
+    {
+        [PrimaryKey]
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+
+        public int Index { get; set; } = 0;
+        public string Title { get; set; }
+        public bool HasContents { get; set; } = false;
+        public bool IsOutWebPage { get; set; } = false;
+        public string Uri { get; set; }
+        public string Contents { get; set; }
+    }
+}
