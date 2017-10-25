929489371e3aca8a3afbb7a999c1f523b5dc99ba
Yusuke Yamada <yamachu.dev@gmail.com>
2017-10-26 01:40:11 +0900

    数字比較のConverterの追加

---
+++ TonpeiFes.Forms/Converters/NumberEqualConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace TonpeiFes.Forms.Converters
+{
+    public class NumberEqualConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!int.TryParse(parameter as string, out int target)) return false;
+            return (int)value == target;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
