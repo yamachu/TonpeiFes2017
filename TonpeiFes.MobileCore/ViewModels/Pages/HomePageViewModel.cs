b3c281e4d1561b9bd630a4fa1f96e2a758f62e64
Yusuke Yamada <yamachu.dev@gmail.com>
2017-10-26 01:40:37 +0900

    HomePageにAnnoucementを表示するようにした

--- TonpeiFes.MobileCore/ViewModels/Pages/HomePageViewModel.cs
+++ TonpeiFes.MobileCore/ViewModels/Pages/HomePageViewModel.cs
@@ -3,15 +3,25 @@ using Prism.Mvvm;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using Reactive.Bindings;
+using TonpeiFes.Core.Models.DataObjects;
+using TonpeiFes.MobileCore.Usecases;
 
 namespace TonpeiFes.MobileCore.ViewModels.Pages
 {
     public class HomePageViewModel : ViewModelBase
     {
         public string Title => "東北大学祭 2017";
-        public HomePageViewModel()
-        {
+        public ReadOnlyReactiveCollection<Announcement> Announcements { get; }
+        private IShowAnnouncement _showAnnouce;
 
+        public HomePageViewModel(IShowAnnouncement showAnnounce)
+        {
+            _showAnnouce = showAnnounce;
+
+            Announcements = _showAnnouce.Announcements.ToReadOnlyReactiveCollection();
+
+            _showAnnouce.InitializeAnnouncements();
         }
     }
 }
