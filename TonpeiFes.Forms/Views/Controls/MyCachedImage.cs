43d4ba78734d5c15660bb1ef8bde5a7a78ae5d9d
Yusuke Yamada <yamachu.dev@gmail.com>
2017-10-27 00:10:03 +0900

    Bindableな感じじゃないからCachedImageを継承したクラスを作成

---
+++ TonpeiFes.Forms/Views/Controls/MyCachedImage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using FFImageLoading.Forms;
+using Xamarin.Forms;
+
+namespace TonpeiFes.Forms.Views.Controls
+{
+    public class MyCachedImage : CachedImage
+    {
+        public MyCachedImage()
+        {
+            RetryCount = 3;
+            RetryDelay = 1000;
+            CacheDuration = TimeSpan.FromDays(30);
+            LoadingPlaceholder = ImageSource.FromFile("loading.png");
+            ErrorPlaceholder = ImageSource.FromFile("loading.png");
+        }
+    }
+}
