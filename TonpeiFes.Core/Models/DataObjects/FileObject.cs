ab566665b152c8f7d6e2ccd3a756d6ac7c989077
Yusuke Yamada <yamachu.dev@gmail.com>
2017-10-27 12:04:24 +0900

    FileObjectの定義を追加

---
+++ TonpeiFes.Core/Models/DataObjects/FileObject.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Realms;
+
+namespace TonpeiFes.Core.Models.DataObjects
+{
+    public class FileObject : RealmObject
+    {
+        [PrimaryKey]
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+
+        [Indexed]
+        public string OriginalName { get; set; }
+
+        public string IconOptimizedFileUrl { get; set; }
+
+        public string DescriptionOptimizedFileUrl { get; set; }
+    }
+}
