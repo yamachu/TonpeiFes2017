cc902440a6aa3a02fe2b8fe46a36ce468991e153
Yusuke Yamada <yamachu.dev@gmail.com>
2017-10-28 16:02:36 +0900

    Realmのサーバーに関係する定数を参照できるInterfaceの追加

---
+++ TonpeiFes.Forms/Configurations/RealmConsts.tmpl.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using TonpeiFes.MobileCore.Configurations;
+
+namespace TonpeiFes.Forms.Configurations
+{
+    public class RealmConsts : IRealmConsts
+    {
+        public string UserId => throw new NotImplementedException();
+
+        public string Password => throw new NotImplementedException();
+
+        public string AuthUrl => throw new NotImplementedException();
+
+        public string DbUri => throw new NotImplementedException();
+    }
+}
---
+++ TonpeiFes.MobileCore/Configurations/IRealmConsts.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace TonpeiFes.MobileCore.Configurations
+{
+    // 今回は共通ユーザを使うことでデータベースを参照できるようにしている
+    // ToDo: 本来の安全な使い方ではないので，もっといい方法を探す
+    public interface IRealmConsts
+    {
+        string UserId { get; }
+        string Password { get; }
+        string AuthUrl { get; }
+        string DbUri { get; }
+    }
+}
