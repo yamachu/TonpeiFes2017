b2dac035eb855a23a157cd27cbd60038e1eb7b9f
Yusuke Yamada <yamachu.dev@gmail.com>
2017-10-28 15:42:25 +0900

    noimageのリソースがAndroidに反映されてなかったのでその修正

--- Droid/Resources/Resource.designer.cs
+++ Droid/Resources/Resource.designer.cs
@@ -4452,26 +4452,26 @@ namespace TonpeiFes.Droid
 			// aapt resource value: 0x7f020053
 			public const int avd_hide_password = 2130837587;
 			
-			// aapt resource value: 0x7f02012c
-			public const int avd_hide_password_1 = 2130837804;
-			
 			// aapt resource value: 0x7f02012d
-			public const int avd_hide_password_2 = 2130837805;
+			public const int avd_hide_password_1 = 2130837805;
 			
 			// aapt resource value: 0x7f02012e
-			public const int avd_hide_password_3 = 2130837806;
+			public const int avd_hide_password_2 = 2130837806;
+			
+			// aapt resource value: 0x7f02012f
+			public const int avd_hide_password_3 = 2130837807;
 			
 			// aapt resource value: 0x7f020054
 			public const int avd_show_password = 2130837588;
 			
-			// aapt resource value: 0x7f02012f
-			public const int avd_show_password_1 = 2130837807;
-			
 			// aapt resource value: 0x7f020130
-			public const int avd_show_password_2 = 2130837808;
+			public const int avd_show_password_1 = 2130837808;
 			
 			// aapt resource value: 0x7f020131
-			public const int avd_show_password_3 = 2130837809;
+			public const int avd_show_password_2 = 2130837809;
+			
+			// aapt resource value: 0x7f020132
+			public const int avd_show_password_3 = 2130837810;
 			
 			// aapt resource value: 0x7f020055
 			public const int common_full_open_on_phone = 2130837589;
@@ -5074,49 +5074,52 @@ namespace TonpeiFes.Droid
 			public const int navigation_empty_icon = 2130837788;
 			
 			// aapt resource value: 0x7f02011d
-			public const int notification_action_background = 2130837789;
+			public const int noimage = 2130837789;
 			
 			// aapt resource value: 0x7f02011e
-			public const int notification_bg = 2130837790;
+			public const int notification_action_background = 2130837790;
 			
 			// aapt resource value: 0x7f02011f
-			public const int notification_bg_low = 2130837791;
+			public const int notification_bg = 2130837791;
 			
 			// aapt resource value: 0x7f020120
-			public const int notification_bg_low_normal = 2130837792;
+			public const int notification_bg_low = 2130837792;
 			
 			// aapt resource value: 0x7f020121
-			public const int notification_bg_low_pressed = 2130837793;
+			public const int notification_bg_low_normal = 2130837793;
 			
 			// aapt resource value: 0x7f020122
-			public const int notification_bg_normal = 2130837794;
+			public const int notification_bg_low_pressed = 2130837794;
 			
 			// aapt resource value: 0x7f020123
-			public const int notification_bg_normal_pressed = 2130837795;
+			public const int notification_bg_normal = 2130837795;
 			
 			// aapt resource value: 0x7f020124
-			public const int notification_icon_background = 2130837796;
-			
-			// aapt resource value: 0x7f02012a
-			public const int notification_template_icon_bg = 2130837802;
-			
-			// aapt resource value: 0x7f02012b
-			public const int notification_template_icon_low_bg = 2130837803;
+			public const int notification_bg_normal_pressed = 2130837796;
 			
 			// aapt resource value: 0x7f020125
-			public const int notification_tile_bg = 2130837797;
+			public const int notification_icon_background = 2130837797;
+			
+			// aapt resource value: 0x7f02012b
+			public const int notification_template_icon_bg = 2130837803;
+			
+			// aapt resource value: 0x7f02012c
+			public const int notification_template_icon_low_bg = 2130837804;
 			
 			// aapt resource value: 0x7f020126
-			public const int notify_panel_notification_icon_bg = 2130837798;
+			public const int notification_tile_bg = 2130837798;
 			
 			// aapt resource value: 0x7f020127
-			public const int segmented_control_background = 2130837799;
+			public const int notify_panel_notification_icon_bg = 2130837799;
 			
 			// aapt resource value: 0x7f020128
-			public const int segmented_control_first_background = 2130837800;
+			public const int segmented_control_background = 2130837800;
 			
 			// aapt resource value: 0x7f020129
-			public const int segmented_control_last_background = 2130837801;
+			public const int segmented_control_first_background = 2130837801;
+			
+			// aapt resource value: 0x7f02012a
+			public const int segmented_control_last_background = 2130837802;
 			
 			static Drawable()
 			{
