7614fefdcbd35895b96cf88d28f16a703dac0811
Yusuke Yamada <yamachu.dev@gmail.com>
2017-10-29 19:33:12 +0900

    中央座標をいい感じにした

--- TonpeiFes.Forms/Configurations/MapAssociated.cs
+++ TonpeiFes.Forms/Configurations/MapAssociated.cs
@@ -5,7 +5,7 @@ namespace TonpeiFes.Forms.Configurations
 {
     public class MapAssociated : IMapAssociated
     {
-        public double MapCenterLangitude { get; } = 38.2599624;
+        public double MapCenterLangitude { get; } = 38.260504;
 
         public double MapCenterLongitude { get; } = 140.8524148;
 
