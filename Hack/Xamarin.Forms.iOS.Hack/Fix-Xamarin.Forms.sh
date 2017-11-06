4e0115c84f453096cd742243b74a5e8f9b42105d
Yusuke Yamada <yamachu.dev@gmail.com>
2017-11-06 22:53:18 +0900

    Add Xamarin.Forms.Platform.iOS hack script
    ref: https://github.com/xamarin/Xamarin.Forms/pull/1244

---
+++ Hack/Xamarin.Forms.iOS.Hack/Fix-Xamarin.Forms.sh
@@ -0,0 +1,3 @@
+#!/bin/bash -e
+
+mono Output/Xamarin.Forms.iOS.Hack.exe ../../packages/Xamarin.Forms.2.4.0.56808/lib/Xamarin.iOS10 /Library/Frameworks/Xamarin.iOS.framework/Versions/Current/lib/64bits ../../packages/Xamarin.Forms.2.4.0.56808/lib/Xamarin.iOS10
